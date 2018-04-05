394b060bb84963ccc1ee3ff2ce69ff9a2f6b39b1
Austen Lamacraft <austen@lamacraft.io>
2018-04-05 20:06:27 +0100

    Connection made

--- ircclient.py
+++ ircclient.py
@@ -1,12 +1,12 @@
 import socket
 
 HOST = "chat.freenode.net"
-PORT = 6697
+PORT = 6667
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
 
-    s.sendall(b"Hello world")
+    s.sendall(b"JOIN :#ldnpydojo")
     data = s.recv(1024)
 
     print(data)
