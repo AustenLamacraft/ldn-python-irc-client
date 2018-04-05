0b701768e288dd67a3b9b80487c7c29b843bff3e
Austen Lamacraft <austen@lamacraft.io>
2018-04-05 20:15:40 +0100

    still not connecting

--- ircclient.py
+++ ircclient.py
@@ -3,10 +3,15 @@ import socket
 HOST = "chat.freenode.net"
 PORT = 6667
 
+def send_data(socket, command):
+    socket.send(command + '\n')
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
 
-    s.sendall(b"JOIN :#ldnpydojo")
+    s.send(b"USER Team4\n")
+    s.send(b"NICK Team4\n")
+    s.send(b"JOIN :#ldnpydojo\n")
     data = s.recv(1024)
 
     print(data)
